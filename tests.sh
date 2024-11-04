efae4744bc9f4ba9693dfa8cfa7ae7d8c5a333a9
Bimochan Shrestha <bmochan@gmail.com>
2024-11-04 19:53:45 +0545

    add-tests: Assert for ignored branches and from .smart-commit-ignore file

--- tests.sh
+++ tests.sh
@@ -43,3 +43,20 @@ run_commit_with_branch() {
     run_commit_with_branch "LPD-1234" "Test message with branch name"
     git log -1 --pretty=%B | grep "LPD-1234: Test message with branch name"
 }
+
+@test "commit does not include branch name for ignored branches" {
+    run_commit_with_branch "dev" "Test message with ignored branches"
+
+    git log -1 --pretty=%B | grep "Test message with ignored branches"
+}
+
+@test "commit does not include branch name for custom ignored branches" {
+    # Add custom ignored branch to .smart-commit-ignore
+    echo "custom-ignore-branch" > ".smart-commit-ignore"
+    git add .smart-commit-ignore
+    git commit -m "Add custom ignore file" > /dev/null
+
+    run_commit_with_branch "custom-ignore-branch" "Test message with custom ignore branch name"
+
+    git log -1 --pretty=%B | grep "Test message with custom ignore branch name"
+}
